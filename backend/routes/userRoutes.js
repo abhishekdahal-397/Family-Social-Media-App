cedd590051ecd615bf02e9417616899970d08329
Abhishek Dahal <abhishekdahal397@gmail.com>
2024-06-30 16:57:32 +0545

    made changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,5 +10,5 @@ router.get("/getUsers", userController.getUsers); // /api/users/getUsers
 router.post("/login", userController.loginUser); // http://localhost:3001/api/users/login
 router.get("/getUser/:id", userController.getUser); // http://localhost:3001/api/users/getUser/:id
 router.get("/:id/profilepic", userController.getUserProfilePic); //http://localhost:3001/api/users/:id/profilePic
-router.get("/getUserPosts/:id", userController.getUserPosts);
+
 module.exports = router;
